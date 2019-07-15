bc65dc895f3de3427f8201d8decf4df9ed4ce201
Thource <reyitgex@gmail.com>
2019-07-15 22:42:47 +0100

    Fixed example paths

--- example/test.lua
+++ example/test.lua
@@ -1,13 +1,13 @@
---@include lib/star_spec.lua
-require("lib/star_spec.lua")
+--@include lib/star_spec/star_spec.lua
+require("lib/star_spec/star_spec.lua")
 
---@includedir lib/star_spec_example/classes
-dodir("lib/star_spec_example/classes")
+--@includedir lib/star_spec/example/classes
+dodir("lib/star_spec/example/classes")
 
 spec(function()
-    --@includedir lib/star_spec_example/specs
-    loadDir("lib/star_spec_example/specs")
+    --@includedir lib/star_spec/example/specs
+    loadDir("lib/star_spec/example/specs")
 
-    -- load("lib/star_spec_example/specs/banana.lua")
-    -- load("lib/star_spec_example/specs/car.lua")
+    -- load("lib/star_spec/example/specs/banana.lua")
+    -- load("lib/star_spec/example/specs/car.lua")
 end)
